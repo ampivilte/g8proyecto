75251cfa2c9c99b69ee167c9093e59e7b17547c1
simonbertran <sbertran@udesa.edu.ar>
2025-09-22 17:48:17 -0300

    complete pagina favoritos

--- proyecto/src/Components/TarjetaPelicula/TarjetaPelicula.js
+++ proyecto/src/Components/TarjetaPelicula/TarjetaPelicula.js
@@ -1,6 +1,6 @@
 import React, {Component}from 'react';
-import { Link } from "react-router-dom";
-import "../../Screens/styles.css";
+import { Link } from "react-router-dom"
+import "../../Screens/styles.css"
 
 
 class TarjetaPelicula extends Component {
@@ -19,35 +19,26 @@ class TarjetaPelicula extends Component {
         this.setState({VerDescripcion:0})
     }
     agregarFav(id){
-        let local = localStorage.getItem('favoritos');
-      
-        if(local !== null){
-          let storageParseado = JSON.parse(local);
-          storageParseado.push(id);
-      
-          // Llamo a la función SOLO si está presente
-          if (this.props.actualizarFav) {
-            this.props.actualizarFav(storageParseado);
-          }
-      
-          let String = JSON.stringify(storageParseado);
-          localStorage.setItem('favoritos', String);
-        } else {
-          let array = [id];
-      
-          if (this.props.actualizarFav) {
-            this.props.actualizarFav(array);
-          }
-      
-          let String = JSON.stringify(array);
-          localStorage.setItem('favoritos', String);
+        let local = localStorage.getItem('favoritos')
+        if(local !==null){
+            let storageParseado = JSON.parse(local)
+            storageParseado.push(id)
+            this.props.actualizarFav(storageParseado,id)
+            let String = JSON.stringify(storageParseado)
+            localStorage.setItem('favoritos',String)
         }
-      }
+        else{
+            let array= [id]
+            this.props.actualizarFav(array,id)
+            let String = JSON.stringify(array)
+            localStorage.setItem('favoritos',String)
+        }
+    }
     sacarFav(id){
         let local = localStorage.getItem('favoritos')
         let storageParseado = JSON.parse(local)
         let filtrado = storageParseado.filter((elm) => elm !== id)
-        this.props.actualizarFav(filtrado)
+        this.props.actualizarFav(filtrado,id)
         let String = JSON.stringify(filtrado)
         localStorage.setItem('favoritos',String)
 
@@ -70,15 +61,14 @@ class TarjetaPelicula extends Component {
                 <div className="cardBody">
                     <h5 class="card-title">{this.props.data.title}</h5>
                     {this.state.VerDescripcion === 0?
-            <button className="btn btn-primary" onClick= {() => this.VerDescripcion()}> Ver descripcion</button>:
-            <button className="btn btn-primary" onClick= {() => this.OcultarDescripcion()}>Ocultar descripcion</button>}
+            <button class="btn btn-primary" onClick= {() => this.VerDescripcion()}> Ver descripcion</button>:
+            <button class="btn btn-primary" onClick= {() => this.OcultarDescripcion()}>Ocultar descripcion</button>}
             {this.state.VerDescripcion === 1?
                 <p>{this.props.data.overview}</p>:
                 <p></p>}
                     
                     <h5> <Link to ={`/detalle/pelicula/${this.props.data.id}`}>Ir a detalle</Link></h5>
-                    {this.props.favorito? <button onClick={()=> this.sacarFav(this.props.data.id)}>Quitar de favoritos</button>:
-                    <button onClick={()=> this.agregarFav(this.props.data.id)}>Agregar a favoritos</button>}
+                    {this.props.favorito? <button onClick={()=> this.sacarFav(this.props.data.id)}>sacar de  favoritos</button>:<button onClick={()=> this.agregarFav(this.props.data.id)}>Agregar a favoritos</button>}
                 </div>
             </article>
 
@@ -87,4 +77,4 @@ class TarjetaPelicula extends Component {
         }
 }
 
-export default TarjetaPelicula;
+export default TarjetaPelicula
--- proyecto/src/Screens/Favoritos/Favoritos.js
+++ proyecto/src/Screens/Favoritos/Favoritos.js
@@ -1,13 +1,10 @@
 import { Component } from "react";
 import TarjetaPelicula from "../../Components/TarjetaPelicula/TarjetaPelicula";
 
-const api_key = 'e4c9de46fa2b077570ba8601a80bf4d6';
-
 class Favoritos extends Component {
     constructor(props){
         super(props)
-        this.state={favoritos: localStorage.getItem('favoritos') !==null ?
-            localStorage.getItem('favoritos'): [],
+        this.state={favoritos: localStorage.getItem('favoritos') !==null ? localStorage.getItem('favoritos'): [],
             pelis:[]
         }
     }
@@ -15,30 +12,38 @@ class Favoritos extends Component {
         if(this.state.favoritos !==null){
             let storage = JSON.parse(this.state.favoritos)
             
-                storage.map((elm =>
-                    fetch(`https://api.themoviedb.org/3/movie${elm}?api_key=${api_key}`)
+                for(let i=0; i< storage.length; i++ ){
+                    let id = storage[i]
+                    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=e4c9de46fa2b077570ba8601a80bf4d6`)
                     .then(res => res.json())
-                    .then(data => this.setState({pelis: this.state.pelis.concat(data)}))
-                    .catch(error => console.log(error))
-                ))
+                    .then(data =>{
+                        let nuevoArray = this.state.pelis ;
+                        nuevoArray.push(data) ;
+                        this.setState({pelis: nuevoArray})
+                    })
+                    .catch(e => console.log(e))
+                }
+                    
+                
         
         }
-        
     }
-    actualizarFav(array){
-        this.setState({favoritos:array})
+    actualizarFav(array,id){
+        let newFav = this.state.pelis.filter(elm => elm.id !== id)
+        this.setState({favoritos:array,pelis:newFav})
     }
     render(){
         return(
             <div>
                 {this.state.pelis !== null ? 
-                    this.state.pelis.map(( elm, idx) => <TarjetaPelicula favorito={this.state.favoritos.includes(elm.id)}  
-                    actualizarFav={(array) => this.actualizarFav(array)}data= {elm}  key={idx + elm.title}/>)
-                        :<h1>No tenes favoritos</h1>}
-
+                 
+                    this.state.pelis.map(( elm, idx) => <TarjetaPelicula favorito={this.state.favoritos.includes(elm.id)}  actualizarFav={(array,id) => this.actualizarFav(array,id)}data= {elm}  key={idx + elm.title}/>)
+                
+                
+                :<h1>No tenes favoritos</h1>}
             </div>
         )
     }
 }
 
-export default Favoritos 
+export default Favoritos
