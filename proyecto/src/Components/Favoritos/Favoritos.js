5a0c6aaf57b64ae0ecf836cb05101677e2526de2
SolAlak <salak@udesa.edu.ar>
2025-09-22 22:03:01 -0300

    arreglo de todo favoritos

--- proyecto/src/Components/Favoritos/Favoritos.js
+++ proyecto/src/Components/Favoritos/Favoritos.js
@@ -1,49 +1,72 @@
 import { Component } from "react";
-import TarjetaPelicula from "../TarjetaPelicula/TarjetaPelicula";
+import TarjetaPelicula from "../../Components/TarjetaPelicula/TarjetaPelicula";
+
+const api_key = 'e4c9de46fa2b077570ba8601a80bf4d6';
 
 class Favoritos extends Component {
-    constructor(props){
-        super(props)
-        this.state={favoritos: localStorage.getItem('favoritos') !==null ? localStorage.getItem('favoritos'): [],
-            pelis:[]
+    constructor(props) {
+      super(props);
+  
+     
+      let favIds = [];
+      if (localStorage.getItem("favoritos") !== null) {
+        favIds = JSON.parse(localStorage.getItem("favoritos"));
+      }
+  
+      this.state = {
+        favoritos: favIds, 
+        pelis: []
+      };
+    }
+  
+    componentDidMount() {
+      
+      for (let i = 0; i < this.state.favoritos.length; i++) {
+        let id = this.state.favoritos[i];
+  
+        if (id) { 
+          fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${api_key}&language=es-AR`)
+            .then(res => res.json())
+            .then(data => {
+              
+              let nuevoArray = this.state.pelis.concat(data);
+              this.setState({ pelis: nuevoArray });
+            })
+            .catch(error => console.log(error));
         }
+      }
     }
-    componentDidMount(){
-        if(this.state.favoritos !==null){
-            let storage = JSON.parse(this.state.favoritos)
-            
-                for(let i=0; i< storage.length; i++ ){
-                    let id = storage[i]
-                    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=e4c9de46fa2b077570ba8601a80bf4d6`)
-                    .then(res => res.json())
-                    .then(data =>{
-                        let nuevoArray = this.state.pelis ;
-                        nuevoArray.push(data) ;
-                        this.setState({pelis: nuevoArray})
-                    })
-                    .catch(e => console.log(e))
-                }
-                    
-                
-        
-        }
+  
+    actualizarFav(arrayActualizado, idQuitado) {
+    
+      let nuevasPelis = this.state.pelis.filter(p => p.id !== idQuitado);
+  
+      this.setState({
+        favoritos: arrayActualizado,
+        pelis: nuevasPelis
+      });
+  
+      localStorage.setItem("favoritos", JSON.stringify(arrayActualizado));
     }
-    actualizarFav(array,id){
-        let newFav = this.state.pelis.filter(elm => elm.id !== id)
-        this.setState({favoritos:array,pelis:newFav})
+  
+    render() {
+      if (this.state.pelis.length === 0) {
+        return <h1>No tenés favoritos</h1>;
+      }
+  
+      return (
+        <div>
+          {this.state.pelis.map((elm) => (
+            <TarjetaPelicula
+              key={elm.id}                
+              data={elm}                  
+              favorito={this.state.favoritos.includes(elm.id)}
+              actualizarFav={(array, id) => this.actualizarFav(array, id)}
+            />
+          ))}
+        </div>
+      );
     }
-    render(){
-        return(
-            <div>
-                {this.state.pelis !== null ? 
-                 
-                    this.state.pelis.map(( elm, idx) => <TarjetaPelicula favorito={this.state.favoritos.includes(elm.id)}  actualizarFav={(array,id) => this.actualizarFav(array,id)}data= {elm}  key={idx + elm.title}/>)
-                
-                
-                :<h1>No tenes favoritos</h1>}
-            </div>
-        )
-    }
-}
-
-export default Favoritos
+  }
+  
+  export default Favoritos;
